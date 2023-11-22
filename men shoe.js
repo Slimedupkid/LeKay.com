091e1159e50ab98c9f290fa083f6d2cd2abc3102
Slimedupkid <147263635+Slimedupkid@users.noreply.github.com>
2023-11-22 19:43:10 +0200

    Add files via upload

---
+++ men shoe.js
@@ -0,0 +1,20 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('.body')
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+let products = [
+    {
+        id: 1,
+        name: 'PRODUCT NAME 1',
+    }
+];
